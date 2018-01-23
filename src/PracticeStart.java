3060b1275d45c1fb0d037666a57b0afb2b77b0bd
javacom00-PC\javacom00 <sundain10@gmail.com>
2018-01-23 13:39:07 +0900

    Scanner 입력 : 입력값에서의 출력:

--- src/PracticeStart.java
+++ src/PracticeStart.java
@@ -1,9 +1,22 @@
+import java.util.Scanner;
 
 public class PracticeStart {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
 		System.out.println("안녕?");
+		
+		
+		//입력
+		System.out.println("숫자를 입력하세요.");
+		int left = sc.nextInt();
+		System.out.println("숫자를 또 입력하세요.");
+		int right = sc.nextInt();
+		
+		//출력
+		System.out.println("입력한 숫자" + left + "," + right);
+		
 
 	}
 
